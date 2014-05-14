ab31b15b2870b0677256ba6a70f061430644f6c0
Allen Chen <jinlong.chen@autodesk.com>
2014-05-14 11:00:58 +0800

    Update color of username and hostname

--- git/git_env.sh
+++ git/git_env.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -
-# Set PS1 prompt with Git repository status
-# cp <this-file> ~/.git_env.sh
-# Put following lines in ~/.bashrc
-# if [ -f ~/.git_env.sh ]; then
-#   source ~/.git_env.sh
-# fi
-
 function parse_git_branch {
     git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
 }
@@ -42,16 +35,19 @@ function short_pwd {
 }
 
 if [ `id -u` = 0  ]; then
-    export COLOR="04;01;31m"
+    export COLOR="01;31m"
     export PATH_COLOR="01;31m"
 else
     export COLOR="01;32m"
     export PATH_COLOR="01;34m"
 fi
 
+export BOLD_WHITE="01;37m"
+export BOLD_CYAN="01;36m"
 export BOLD_RED="01;31m"
 export BOLD_GREEN="01;32m"
 export BOLD_BLUE="01;34m"
+export BOLD_PURPLE="01;35m"
 
-export PS1='\[\033[$COLOR\]\u@\h\[\033[00m\]:\[\033[01;$PATH_COLOR\]\W\[\033[00m\]\[\033[01;35m\]$(parse_git_branch)\[\033[00m\]\[\033[$BOLD_RED\]$(git_unadded_new)\[\033[00m\]\[\033[$BOLD_GREEN\]$(git_needs_commit)\[\033[00m\]\[\033[$BOLD_BLUE\]$(git_modified_files)\[\033[00m\]> '
+export PS1='\[\033[$BOLD_CYAN\]\u@\[\033[$BOLD_PURPLE\]\h\[\033[00m\]:\[\033[01;$PATH_COLOR\]\W\[\033[00m\]\[\033[01;35m\]$(parse_git_branch)\[\033[00m\]\[\033[$BOLD_RED\]$(git_unadded_new)\[\033[00m\]\[\033[$BOLD_GREEN\]$(git_needs_commit)\[\033[00m\]\[\033[$BOLD_BLUE\]$(git_modified_files)\[\033[00m\]> '
 # vim: tabstop=4 shiftwidth=4 autoindent
