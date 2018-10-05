0c6b89414fd0fe3b7bc62afae4c5d1ab582bcde9
Allen Chen <a3linux@gmail.com>
2018-10-05 09:56:45 +0800

    -Add generate QRCode script

---
+++ ss/service/gen_qr.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import qrcode
+import base64
+import json
+import argparse
+from os import path
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", dest="conf_file", required=True)
+    parser.add_argument("-s", "--server", dest="server_address", required=True)
+
+    args = parser.parse_args()
+
+    if args.conf_file and path.isfile(args.conf_file):
+        try:
+            with open(args.conf_file, "r") as f:
+                conf = json.load(f)
+            ss_config_str = "%s:%s@%s:%s" % (conf["method"], conf["password"],
+                                                  args.server_address,
+                                                  conf["server_port"])
+            print(ss_config_str)
+            print(base64.b64encode(ss_config_str.encode('ascii')).decode('ascii'))
+            img = qrcode.make("ss://%s" %
+                              base64.b64encode(
+                                  ss_config_str.encode('ascii')).decode('ascii'))
+            img.save("%s.png" % conf["server_port"])
+        except Exception as exp:
+            print("Error when generating QRCode %s" % exp)
